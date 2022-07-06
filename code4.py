0af3effd4d702fbff8b2ef8f8850b619ca2a3d81
“Igormirowski” <igorpmirowski@gmail.com>
2022-07-06 07:47:49 +0100

    a

--- code4.py
+++ code4.py
@@ -12,3 +12,24 @@ print(aisle_c)
 
 for item in aisle_c:
     print(item)
+
+
+## DICTIONARY 
+
+dic1 = {'a': 1,'b': 2, 'c': 3 }
+
+#see keys 
+for elt in dic1:
+    print(elt)
+
+#see values
+for elt in dic1.items():
+    print(elt)
+
+for(key, value) in dic1.items():
+    print(f' key is {key} ad vaule is {value}')
+
+# Multiply value by 2 (DICTIONARY COMPREHENTION) DOUBLE THE VALUE 
+
+double_dic1 = {k:v*2 for (k, v) in dic1.items()}
+print(double_dic1)
